c7d7ae636c479c787391a32be5f04006e2d2bcaf
Van Nguyen <thegoleffect@gmail.com>
2012-03-19 10:26:31 -0700

    refactor some cpu parts, added more memory code

--- test/test.monitor.js
+++ test/test.monitor.js
@@ -4,6 +4,9 @@ var should = require("should");
 describe("monitor", function(){
   describe('#cpu', function(){
     it("should", function(done){
+      var mem = monitor.memory();
+      console.log(mem);
+      
       var output = monitor.cpu(function(err, response){
         done();
       });
