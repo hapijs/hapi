ae22a4cc2def8e0656ad9af9c621a23abdf53561
Van Nguyen <thegoleffect@gmail.com>
2012-03-19 12:06:19 -0700

    starting to add testing

--- test/test.monitor.js
+++ test/test.monitor.js
@@ -3,13 +3,35 @@ var should = require("should");
 
 describe("monitor", function(){
   describe('#cpu', function(){
-    it("should", function(done){
-      var mem = monitor.memory();
-      console.log(mem);
-      
-      var output = monitor.cpu(function(err, response){
+    it("should return a fixed-decimal percentage string", function(done){
+      var output = monitor.cpu(function(err, percentage){
+        should.not.exist(err);
+        should.exist(percentage);
+        percentage.should.have.length(4);
         done();
       });
     })
   })
+  
+  describe("#memory", function() {
+    it("should return used & total values", function(done){
+      var output = monitor.memory();
+      
+      should.exist(output);
+      should.exist(output.total);
+      should.exist(output.used);
+      
+      done();
+    })
+  });
+  
+  describe("#uptime", function(){
+    it("should return ", function(done){
+      var uptime = monitor.uptime();
+      should.exist(uptime);
+      done();
+    })
+  })
+  
+  // describe("")
 })
