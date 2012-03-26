a1cc9d63677a74536d2cf1921010e4d2fe1dccf4
Van Nguyen <thegoleffect@gmail.com>
2012-03-26 14:32:05 -0700

    tests for monitor

--- test/test.monitor.js
+++ test/test.monitor.js
@@ -1,27 +1,21 @@
+var async = require("async");
 var monitor = require("../lib/monitor");
 var should = require("should");
 
-
 describe("monitor", function(){
   describe("#os", function(){
-    // // Builtins
-    // var os = require("os");
-    // for(var key in os){
-    //   console.log(key)
-    //   if (key == 'getNetworkInterfaces'){ continue; }
-      
-    //   if (os.hasOwnProperty(key) && typeof os[key] == "function"){
-    //     describe('#' + key, function(){
-    //       it("should return without error", function(done){
-    //         monitor.os[key](function(err, response){
-    //           should.not.exist(err);
-    //           should.exist(response);
-    //           done();
-    //         })
-    //       })
-    //     })
-    //   }
-    // }
+    // Builtins
+    it("should expose some builtins", function(done){
+      async.parallel(monitor.os.builtins.map(function(d){ return monitor.os[d] }), function(err, result){
+        should.not.exist(err);
+        
+        for(var key in monitor.os.builtins){
+          should.exist(result[key])
+        }
+        
+        done();
+      })
+    })
     
     describe('#cpu', function(){
       it("should return a fixed-decimal percentage string", function(done){
@@ -40,9 +34,33 @@ describe("monitor", function(){
         })
       })
     })
+    
+    describe("#disk", function(){
+      it('should return without error', function(done){
+        monitor.os.disk(function(err, response){
+          should.not.exist(err);
+          should.exist(response);
+          
+          done();
+        })
+      })
+    })
   })
   
   describe("#process", function(){
+    // Builtins
+    it("should expose some builtins", function(done){
+      async.parallel(monitor.process.builtins.map(function(d){ return monitor.process[d] }), function(err, result){
+        should.not.exist(err);
+        
+        for(var key in monitor.process.builtins){
+          should.exist(result[key])
+        }
+        
+        done();
+      })
+    })
+    
     describe("#cpu", function(){
       it("should return a string", function(done){
         monitor.process.cpu(function(err, percentage){
@@ -53,74 +71,14 @@ describe("monitor", function(){
       })
     })
     
-    describe("#uptime", function(){
+    describe("#memory", function(){
       it("should return without error", function(done){
-        monitor.process.uptime(function(err, uptime){
+        monitor.process.memory(function(err, memory){
           should.not.exist(err);
-          should.exist(uptime);
+          should.exist(memory);
           done();
         })
       })
     })
   })
-  
-  // describe("#pcpu", function(){
-  //   it("should return a string", function(done){
-  //     monitor.pcpu(function(err, percentage) {
-  //       should.not.exist(err);
-  //       should.exist(percentage);
-        
-  //       done()
-  //     })
-  //   })
-  // })
-  
-  // describe("#memory", function() {
-  //   it("should return non null value", function(done){
-  //     var output = monitor.memory();
-      
-  //     should.exist(output);
-  //     done();
-  //   })
-    
-  //   it('should contain total RAM', function(done){
-  //     var output = monitor.memory();
-      
-  //     should.exist(output.total);
-  //     done();
-  //   })
-    
-  //   it('should contain total rss RAM', function(done){
-  //     var output = monitor.memory();
-      
-  //     should.exist(output.rss);
-  //     done();
-  //   })
-    
-  //   it('should contain total heap RAM', function(done){
-  //     var output = monitor.memory();
-      
-  //     should.exist(output.heap);
-  //     done();
-  //   })
-  // });
-  
-  // describe("#uptime", function(){
-  //   it("should return ", function(done){
-  //     var uptime = monitor.uptime();
-  //     should.exist(uptime);
-  //     done();
-  //   })
-  // })
-  
-  // describe("#disk", function(){
-  //   it("should return valid percentage for default filesystem", function(done){
-  //     monitor.disk('/', function(err, percentage){
-  //       should.not.exist(err)
-  //       should.exist(percentage)
-  //       percentage.should.be.a("string")
-  //       done();
-  //     });
-  //   })
-  // })
 })
