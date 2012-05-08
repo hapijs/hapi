c7c93f7aa69980fe0eafab07bfe87f028add7aa6
Van Nguyen <thegoleffect@gmail.com>
2012-05-08 13:43:57 -0700

    monitoring ops, request and scaffold integration test

---
+++ test/integration.hapi.server.js
@@ -0,0 +1,59 @@
+var request = require('request');
+var should = require("should");
+var Hapi = require("../index");
+
+var testHandler = function(hapi, reply) {
+
+    reply("ohai");
+}
+
+describe("Hapi", function() {
+
+    describe(".Server", function() {
+
+        describe("#Server", function() {
+
+            var host = "localhost",
+                routes = [
+                    { method: 'GET',  path: '/', handler: testHandler, tos: 'none', authentication: 'none' }
+                ];
+
+            it("should initialize, start, and stop", function(done) {
+                var port = 3000,
+                    options = {
+                        name: 'Test API Server'
+                    };
+
+                Hapi.Process.initialize(options);
+                var s = new Hapi.Server.Server(host, port, options, routes);
+                s.start();
+
+                request('http://' + host + ":" + port + "/", function(err, r, body) {
+
+                    r.statusCode.should.equal(200);
+                    body.should.equal("ohai");
+                    s.stop();
+                    done();
+                })
+            })
+
+            // it("should run fine with log monitoring", function(done){
+            //     var port = 3001,
+            //         options = {
+            //             name: 'Test API Server',
+            //             monitor: {
+            //                 log: {
+            //                     "http://localhost:9000/analytics/log": 1
+            //                 },
+            //                 interval: 15000
+            //             }
+            //         };
+                
+            //     Hapi.Process.initialize(options)
+            //     var s = new Hapi.Server.Server(host, port, options, routes);
+            //     s.start()
+                
+            // })
+        })
+    })
+})
