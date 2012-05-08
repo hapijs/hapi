ea0da93572dfd1458b20391be423870f6e89f73d
Van Nguyen <thegoleffect@gmail.com>
2012-05-08 10:30:38 -0700

    format test using Eran-style

--- test/hapi.server.js
+++ test/hapi.server.js
@@ -1,35 +1,41 @@
 var should = require("should");
 var Hapi = require("../index");
 
-var testHandler = function(hapi, reply){
-  reply("ohai")
+var testHandler = function(hapi, reply) {
+
+    reply("ohai")
 }
 
-describe("Hapi", function(){
-  describe(".Server", function(){
-    describe("#Server", function(){
-      it("should initialize, start, and stop", function(done){
-        var host = "localhost",
-          port = 3000,
-          options = {
-            
-          },
-          routes = [
-            { method: 'GET',  path: '/', handler: testHandler, tos: 'none', authentication: 'none' }
-          ];
-        
-        Hapi.Process.initialize({
-          name: 'Test API Server'
+describe("Hapi", function() {
+
+    describe(".Server", function() {
+
+        describe("#Server", function() {
+
+            it("should initialize, start, and stop", function(done) {
+                var host = "localhost",
+                    port = 3000,
+                    options = {
+                      
+                    },
+                    routes = [
+                        { method: 'GET',  path: '/', handler: testHandler, tos: 'none', authentication: 'none' }
+                    ];
+
+                Hapi.Process.initialize({
+
+                    name: 'Test API Server'
+                });
+                
+                var s = new Hapi.Server.Server(host, port, options, routes);
+                s.start();
+
+                setTimeout((function() {
+
+                    s.stop();
+                    done();
+                }), 150);
+            })
         })
-        
-        var s = new Hapi.Server.Server(host, port, options, routes);
-        s.start()
-        
-        setTimeout((function(){
-          s.stop();
-          done();
-        }), 150)
-      })
     })
-  })
 })
