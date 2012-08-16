e66c0321e67f74c0b68c2308292cb36f52278806
Van Nguyen <thegoleffect@gmail.com>
2012-08-16 10:33:05 -0700

    block off string/number test so i know to add tests for other types

--- test/unit/validation.js
+++ test/unit/validation.js
@@ -12,51 +12,53 @@ var OhaiHandler = function (hapi, reply) {
 
 describe("Validation", function(){
   describe("#validateQuery", function(){
-    var OhaiRoute = { method: 'GET', path: '/', handler: OhaiHandler, query: {name: S().min(2).max(10), age: N().min(0)} }
-    var validOhaiQuery = {
-      name: "van",
-      age: ""+new Date().getFullYear() - 1985
-    }
-    var validOhaiQStr = qs.stringify(validOhaiQuery);
-    var validIncomingRequest = {
-      httpVersion: '1.1',
-      url: '/?' + validOhaiQStr,
-      method: 'GET',
-      hapi: {
-        server: {},
+    describe("string and number types", function(){
+      var OhaiRoute = { method: 'GET', path: '/', handler: OhaiHandler, query: {name: S().min(2).max(10), age: N().min(0)} }
+      var validOhaiQuery = {
+        name: "van",
+        age: ""+new Date().getFullYear() - 1985
+      }
+      var validOhaiQStr = qs.stringify(validOhaiQuery);
+      var validIncomingRequest = {
+        httpVersion: '1.1',
         url: '/?' + validOhaiQStr,
-        query: validOhaiQuery,
-        params: {}
+        method: 'GET',
+        hapi: {
+          server: {},
+          url: '/?' + validOhaiQStr,
+          query: validOhaiQuery,
+          params: {}
+        }
+      };
+      var invalidOhaiQuery = {
+        name: "v",
+        age: "x"
       }
-    };
-    var invalidOhaiQuery = {
-      name: "v",
-      age: "x"
-    }
-    var invalidOhaiQStr = qs.stringify(invalidOhaiQuery);
-    var invalidIncomingRequest = {
-      httpVersion: '1.1',
-      url: '/?' + invalidOhaiQStr,
-      method: 'GET',
-      hapi: {
-        server: {},
+      var invalidOhaiQStr = qs.stringify(invalidOhaiQuery);
+      var invalidIncomingRequest = {
+        httpVersion: '1.1',
         url: '/?' + invalidOhaiQStr,
-        query: invalidOhaiQuery,
-        params: {}
-      }
-    };
-    
-    it("should validate known working configuration", function(done){
-      Validation.validateQuery(validIncomingRequest, OhaiRoute.query, function(err){
-        should.not.exist(err);
-        done();
+        method: 'GET',
+        hapi: {
+          server: {},
+          url: '/?' + invalidOhaiQStr,
+          query: invalidOhaiQuery,
+          params: {}
+        }
+      };
+      
+      it("should validate known working configuration", function(done){
+        Validation.validateQuery(validIncomingRequest, OhaiRoute.query, function(err){
+          should.not.exist(err);
+          done();
+        })
       })
-    })
-    
-    it("should invalid known non-working configuration", function(done){
-      Validation.validateQuery(invalidIncomingRequest, OhaiRoute.query, function(err){
-        should.exist(err);
-        done();
+      
+      it("should invalid known non-working configuration", function(done){
+        Validation.validateQuery(invalidIncomingRequest, OhaiRoute.query, function(err){
+          should.exist(err);
+          done();
+        })
       })
     })
   })
