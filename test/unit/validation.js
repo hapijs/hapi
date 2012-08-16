59e4fa84efa0d7b74cec83eb887203c074b752b2
Van Nguyen <thegoleffect@gmail.com>
2012-08-16 10:59:08 -0700

    starting to refactor tests into usable sections

--- test/unit/validation.js
+++ test/unit/validation.js
@@ -10,42 +10,63 @@ var OhaiHandler = function (hapi, reply) {
   reply('ohai');
 };
 
+var createRequestObject = function(query){
+  var qstr = qs.stringify(query);
+  return {
+    httpVersion: '1.1',
+    url: '/?' + qstr,
+    method: 'GET',
+    hapi: {
+      server: {},
+      url: '/?' + qstr,
+      query: query,
+      params: {}
+    }
+  }
+}
+
 describe("Validation", function(){
   describe("#validateQuery", function(){
+    describe("using Types.String", function(){
+      describe("#required", function(){
+        var route = {method: 'GET', path: '/', handler: OhaiHandler, query: {username: S().required()}};
+        
+        it("should raise error on undefined REQUIRED parameter", function(done){
+          var query = {}
+          var request = createRequestObject(query);
+          
+          Validation.validateQuery(request, route.query, function(err){
+            should.exist(err);
+            done();
+          })
+        })
+        
+        it('should not raise error on defined REQUIRED parameter', function(done){
+          var query = {username: "walmart"}
+          var request = createRequestObject(query);
+          
+          Validation.validateQuery(request, route.query, function(err){
+            should.exist(err);
+            done();
+          })
+        })
+      })
+      
+    })
+    
+    // TODO: deprecate these
     describe("string and number types", function(){
       var OhaiRoute = { method: 'GET', path: '/', handler: OhaiHandler, query: {name: S().min(2).max(10), age: N().min(0)} }
       var validOhaiQuery = {
         name: "van",
         age: ""+new Date().getFullYear() - 1985
       }
-      var validOhaiQStr = qs.stringify(validOhaiQuery);
-      var validIncomingRequest = {
-        httpVersion: '1.1',
-        url: '/?' + validOhaiQStr,
-        method: 'GET',
-        hapi: {
-          server: {},
-          url: '/?' + validOhaiQStr,
-          query: validOhaiQuery,
-          params: {}
-        }
-      };
+      var validIncomingRequest = createRequestObject(validOhaiQuery);
       var invalidOhaiQuery = {
         name: "v",
         age: "x"
       }
-      var invalidOhaiQStr = qs.stringify(invalidOhaiQuery);
-      var invalidIncomingRequest = {
-        httpVersion: '1.1',
-        url: '/?' + invalidOhaiQStr,
-        method: 'GET',
-        hapi: {
-          server: {},
-          url: '/?' + invalidOhaiQStr,
-          query: invalidOhaiQuery,
-          params: {}
-        }
-      };
+      var invalidIncomingRequest = createRequestObject(invalidOhaiQuery)
       
       it("should validate known working configuration", function(done){
         Validation.validateQuery(validIncomingRequest, OhaiRoute.query, function(err){
