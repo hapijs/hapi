ad15c061964166578210405700cfac6308597c2d
Eran Hammer <eran@hueniverse.com>
2012-10-15 00:39:23 -0700

    Test Log

--- test/unit/log.js
+++ test/unit/log.js
@@ -4,24 +4,35 @@ var expect = require('chai').expect;
 var Log = process.env.TEST_COV ? require('../../lib-cov/log') : require('../../lib/log');
 
 
-describe('Log', function() {
+describe('Log', function () {
 
-    describe('#event', function() {
+    describe('#event', function () {
 
-        it('fires an event with the passed in tags', function(done) {
-            var env = process.env.NODE_ENV;
-            var tags = ['hello'];
-            process.env.NODE_ENV = 'nottatest';
+        it('fires an event with the passed in tags', function (done) {
 
-            Log.on('log', function(event) {
+            Log.logFunc = function (string) {
+
+                expect(string.match(/^\*\d{6}\/\d{6}\.\d{1,3}\, test, (\{"1"\:2\})|(JSON Error\: .*)$/)).to.exist;
+            };
+
+            Log.isTesting = true;
+            Log.event('test', { 1: 2 });
+            var circ = {};
+            circ.circ = circ;
+            Log.event('test', circ);
+
+            Log.on('log', function (event) {
+
                 expect(event).to.exist;
                 expect(event.tags).to.exist;
                 expect(event.tags[0]).to.equal('hello');
+
+                Log.isTesting = false;
+                Log.logFunc = console.log;
                 done();
             });
-            Log.event(tags, null, Date.now());
-
-            process.env.NODE_ENV = env;
+            Log.event(['hello'], null, Date.now());
         });
     });
 });
+
