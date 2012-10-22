6ec76eb1282e7cfc771981b09f6e545ad19902f6
Eran Hammer <eran@hueniverse.com>
2012-10-22 00:29:05 -0700

    Initial proxy support

---
+++ examples/config/proxy.js
@@ -0,0 +1,25 @@
+// Load modules
+
+var Hapi = require('../../lib/hapi');
+
+
+// Declare internals
+
+var internals = {};
+
+
+internals.main = function () {
+
+    // Create Hapi servers
+    var http = new Hapi.Server('0.0.0.0', 8080);
+
+    // Set routes
+    http.addRoute({ method: 'GET', path: '/', config: { proxy: { host: 'google.com', port: 80 } } });
+
+    // Start Hapi servers
+    http.start();
+};
+
+
+internals.main();
+
