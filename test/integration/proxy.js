db744bd755f46981f605a0a1fcb012e6a2563bae
Wyatt Preul <wpreul@gmail.com>
2012-10-23 17:08:48 -0500

    Fixing proxy to correctly pipe responses

--- test/integration/proxy.js
+++ test/integration/proxy.js
@@ -1,32 +1,74 @@
 // Load modules
 
 var expect = require('chai').expect;
-var Sinon = require('sinon');
-var Async = require('async');
 var Hapi = process.env.TEST_COV ? require('../../lib-cov/hapi') : require('../../lib/hapi');
+var Request = require('request');
 
 describe('Proxy', function() {
 
     before(startServer);
 
     var _server = null;
-    var _serverUrl = 'http://127.0.0.1:18099';
+    var _serverUrl = 'http://127.0.0.1:18092';
 
     function startServer(done) {
 
-        _server = new Hapi.Server('0.0.0.0', 18099);
+        var listening = false;
+        var dummyServer = new Hapi.Server('0.0.0.0', 18093);
+        dummyServer.addRoutes([{ method: 'GET', path: '/profile', config: { handler: profile } },
+            { method: 'GET', path: '/item', config: { handler: activeItem } },
+            { method: 'POST', path: '/item', config: { handler: item } }]);
+
+        _server = new Hapi.Server('0.0.0.0', 18092);
         _server.addRoutes([
-            { method: 'GET', path: '/', config: { proxy: { host: 'google.com', port: 80 } } },
-            { method: 'POST', path: '/', config: { proxy: { host: 'google.com', port: 80 } } }
+            { method: 'GET', path: '/profile', config: { proxy: { host: '127.0.0.1', port: 18093 } } },
+            { method: 'POST', path: '/item', config: { proxy: { host: '127.0.0.1', port: 18093 } } }
         ]);
-        _server.listener.on('listening', function() {
-            done();
+
+        dummyServer.listener.on('listening', function() {
+            if (listening) {
+                done();
+            }
+            else {
+                listening = true;
+            }
         });
+        _server.listener.on('listening', function() {
+            if (listening) {
+                done();
+            }
+            else {
+                listening = true;
+            }
+        });
+
+        dummyServer.start();
         _server.start();
     }
 
+    function profile(request) {
+        request.reply({
+            'id': 'fa0dbda9b1b',
+            'name': 'John Doe'
+        });
+    }
+
+    function activeItem(request) {
+        request.reply({
+            'id': '55cf687663',
+            'name': 'Active Item'
+        });
+    }
+
+    function item(request) {
+        request.reply.created('http://google.com')({
+            'id': '55cf687663',
+            'name': 'Item'
+        });
+    }
+
     function makeRequest(options, callback) {
-        var next = function(res) {
+        var next = function(err, res) {
             return callback(res);
         };
 
@@ -34,23 +76,24 @@ describe('Proxy', function() {
         options.path = options.path || '/';
         options.method = options.method || 'get';
 
-        _server.inject({
+       Request({
             method: options.method,
             url: _serverUrl + options.path
         }, next);
     }
 
     it('forwards on the response when making a GET request', function(done) {
-        this.timeout(4000);
-        makeRequest(null, function(rawRes) {
+        makeRequest({ path: '/profile' }, function(rawRes) {
             expect(rawRes.statusCode).to.equal(200);
+            expect(rawRes.body).to.contain('John Doe');
             done();
         });
     });
 
-    it('forwards on the response when making a POST request', function(done) {
-        makeRequest({ method: 'post' }, function(rawRes) {
-            expect(rawRes.statusCode).to.equal(405);
+    it('forwards on the status code when making a POST request', function(done) {
+        makeRequest({ path: '/item', method: 'post' }, function(rawRes) {
+            expect(rawRes.statusCode).to.equal(201);
+            expect(rawRes.body).to.contain('Item');
             done();
         });
     });
