359a81997e56c05bfe1c5f00621ddbf56611a1b3
Wyatt Preul <wpreul@gmail.com>
2012-11-02 12:53:30 -0500

    Adding proxy tests and doing a little refactoring

--- test/integration/proxy.js
+++ test/integration/proxy.js
@@ -14,10 +14,9 @@ describe('Proxy', function () {
     function startServer(done) {
 
         var listening = false;
-        var config = null;
 
         var routeCache = {
-            mode: 'server',
+            mode: 'server+client',
             expiresIn: 500
         };
 
@@ -32,13 +31,20 @@ describe('Proxy', function () {
         var dummyServer = new Hapi.Server('0.0.0.0', 18093);
         dummyServer.addRoutes([{ method: 'GET', path: '/profile', config: { handler: profile } },
             { method: 'GET', path: '/item', config: { handler: activeItem } },
-            { method: 'POST', path: '/item', config: { handler: item } }]);
+            { method: 'POST', path: '/item', config: { handler: item } },
+            { method: 'GET', path: '/unauthorized', config: { handler: unauthorized }},
+            { method: 'POST', path: '/echo', config: { handler: echoPostBody } }
+        ]);
 
         _server = new Hapi.Server('0.0.0.0', 18092, config);
         _server.addRoutes([
             { method: 'GET', path: '/profile', config: { proxy: { host: '127.0.0.1', port: 18093, xforward: true, passThrough: true } } },
             { method: 'GET', path: '/item', config: { proxy: { host: '127.0.0.1', port: 18093 }, cache: routeCache } },
-            { method: 'POST', path: '/item', config: { proxy: { host: '127.0.0.1', port: 18093 } } }
+            { method: 'GET', path: '/unauthorized', config: { proxy: { host: '127.0.0.1', port: 18093 }, cache: routeCache } },
+            { method: 'POST', path: '/item', config: { proxy: { host: '127.0.0.1', port: 18093 } } },
+            { method: 'POST', path: '/notfound', config: { proxy: { host: '127.0.0.1', port: 18093 } } },
+            { method: 'GET', path: '/postResponseError', config: { proxy: { host: '127.0.0.1', port: 18093, postResponse: postResponseWithError }, cache: routeCache } },
+            { method: 'POST', path: '/echo', config: { proxy: { host: '127.0.0.1', port: 18093 } } }
         ]);
 
         dummyServer.listener.on('listening', function () {
@@ -63,6 +69,7 @@ describe('Proxy', function () {
     }
 
     function profile(request) {
+
         request.reply({
             'id': 'fa0dbda9b1b',
             'name': 'John Doe'
@@ -70,6 +77,7 @@ describe('Proxy', function () {
     }
 
     function activeItem(request) {
+
         request.reply({
             'id': '55cf687663',
             'name': 'Active Item'
@@ -77,13 +85,36 @@ describe('Proxy', function () {
     }
 
     function item(request) {
+
         request.reply.created('http://google.com')({
             'id': '55cf687663',
             'name': 'Item'
         });
     }
 
+    function echoPostBody(request) {
+
+        request.reply(request.payload);
+    }
+
+    function unauthorized(request) {
+
+        request.reply(Hapi.Error.unauthorized('Not authorized'));
+    }
+
+    function postResponseWithError(request) {
+
+        request.reply(Hapi.Error.forbidden('Forbidden'));
+    }
+
+    function postResponse(request, settings, response, payload) {
+
+        request.reply.type(response.headers['content-type']);
+        request.reply(payload);
+    }
+
     function makeRequest(options, callback) {
+
         var next = function (err, res) {
             return callback(res);
         };
@@ -94,12 +125,15 @@ describe('Proxy', function () {
 
         Request({
             method: options.method,
-            url: _serverUrl + options.path
+            url: _serverUrl + options.path,
+            form: options.form
         }, next);
     }
 
     it('forwards on the response when making a GET request', function (done) {
+
         makeRequest({ path: '/profile' }, function (rawRes) {
+
             expect(rawRes.statusCode).to.equal(200);
             expect(rawRes.body).to.contain('John Doe');
             done();
@@ -107,7 +141,9 @@ describe('Proxy', function () {
     });
 
     it('forwards on the response when making a GET request to a route that also accepts a POST', function (done) {
+
         makeRequest({ path: '/item' }, function (rawRes) {
+
             expect(rawRes.statusCode).to.equal(200);
             expect(rawRes.body).to.contain('Active Item');
             done();
@@ -115,9 +151,57 @@ describe('Proxy', function () {
     });
 
     it('forwards on the status code when making a POST request', function (done) {
+
         makeRequest({ path: '/item', method: 'post' }, function (rawRes) {
+
             expect(rawRes.statusCode).to.equal(201);
             expect(rawRes.body).to.contain('Item');
+            done();
+        });
+    });
+
+    it('sends the correct status code with a request is unauthorized', function(done) {
+
+        makeRequest({ path: '/unauthorized', method: 'get' }, function (rawRes) {
+
+            expect(rawRes.statusCode).to.equal(401);
+            done();
+        });
+    });
+
+    it('sends a 404 status code with a proxied route doesn\'t exist', function(done) {
+
+        makeRequest({ path: '/notfound', method: 'get' }, function (rawRes) {
+
+            expect(rawRes.statusCode).to.equal(404);
+            done();
+        });
+    });
+
+    it('forwards on the status code when a custom postResponse returns an error', function(done) {
+
+        makeRequest({ path: '/postResponseError', method: 'get' }, function (rawRes) {
+
+            expect(rawRes.statusCode).to.equal(403);
+            done();
+        });
+    });
+
+    it('forwards the error message with a custom postResponse and a route error', function(done) {
+
+        makeRequest({ path: '/postResponseNotFound', method: 'get' }, function (rawRes) {
+
+            expect(rawRes.body).to.contain('error');
+            done();
+        });
+    });
+
+    it('forwards on a POST body', function(done) {
+
+        makeRequest({ path: '/echo', method: 'post', form: { echo: true } }, function (rawRes) {
+
+            expect(rawRes.statusCode).to.equal(200);
+            expect(rawRes.body).to.contain('echo');
             done();
         });
     });
