217bb6b4329eebbdb5d9be89b324e0b14dd310b2
Eran Hammer <eran@hueniverse.com>
2012-11-07 13:03:39 -0800

    Remove files

--- test/unit/session.js
+++
@@ -1,9 +0,0 @@
-// Load modules
-
-var expect = require('chai').expect;
-var Session = process.env.TEST_COV ? require('../../lib-cov/session') : require('../../lib/session');
-
-
-describe('Session', function() {
-
-});
