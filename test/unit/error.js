7511f120337db4869da3ad2077fa1b7b26d1edbe
Eran Hammer <eran@hueniverse.com>
2012-11-15 00:20:16 -0800

    Error test

--- test/unit/error.js
+++ test/unit/error.js
@@ -1,73 +1,96 @@
 var expect = require('chai').expect;
 var Err = process.env.TEST_COV ? require('../../lib-cov/error') : require('../../lib/error');
 
-describe('Err', function() {
 
-    describe('#badRequest', function() {
+describe('Err', function () {
 
-        it('returns a 400 error code', function(done) {
+    describe('#Error', function () {
+
+        it ('returns an error with info when constructed using another error and message', function (done) {
+
+            var err = new Err(new Error('inner'), 'outter');
+            expect(err.message).to.equal('inner');
+            expect(err.info).to.equal('outter');
+            done();
+        });
+    });
+
+    describe('#badRequest', function () {
+
+        it('returns a 400 error code', function (done) {
+
             expect(Err.badRequest().code).to.equal(400);
             done();
         });
 
-        it('sets the message with the passed in message', function(done) {
+        it('sets the message with the passed in message', function (done) {
+
             expect(Err.badRequest('my message').message).to.equal('my message');
             done();
         });
     });
 
-    describe('#unauthorized', function() {
+    describe('#unauthorized', function () {
 
-        it('returns a 401 error code', function(done) {
+        it('returns a 401 error code', function (done) {
+
             expect(Err.unauthorized().code).to.equal(401);
             done();
         });
 
-        it('sets the message with the passed in message', function(done) {
+        it('sets the message with the passed in message', function (done) {
+
             expect(Err.unauthorized('my message').message).to.equal('my message');
             done();
         });
     });
 
-    describe('#forbidden', function() {
+    describe('#forbidden', function () {
 
-        it('returns a 403 error code', function(done) {
+        it('returns a 403 error code', function (done) {
+
             expect(Err.forbidden().code).to.equal(403);
             done();
         });
 
-        it('sets the message with the passed in message', function(done) {
+        it('sets the message with the passed in message', function (done) {
+
             expect(Err.forbidden('my message').message).to.equal('my message');
             done();
         });
     });
 
-    describe('#notFound', function() {
+    describe('#notFound', function () {
 
-        it('returns a 404 error code', function(done) {
+        it('returns a 404 error code', function (done) {
+
             expect(Err.notFound().code).to.equal(404);
             done();
         });
 
-        it('sets the message with the passed in message', function(done) {
+        it('sets the message with the passed in message', function (done) {
+
             expect(Err.notFound('my message').message).to.equal('my message');
             done();
         });
     });
 
-    describe('#internal', function() {
+    describe('#internal', function () {
 
-        it('returns a 500 error code', function(done) {
+        it('returns a 500 error code', function (done) {
+
             expect(Err.internal().code).to.equal(500);
             done();
         });
 
-        it('sets the message with the passed in message', function(done) {
+        it('sets the message with the passed in message', function (done) {
+
             expect(Err.internal('my message').message).to.equal('my message');
             done();
         });
 
-        it('passes data on the callback if its passed in', function(done) {
+        it('passes data on the callback if its passed in', function (done) {
+
             expect(Err.internal('my message', { my: 'data' }).data.my).to.equal('data');
             done();
         });
