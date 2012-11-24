e5f1f8ffecf2c3143103c3a279c278c608481b6c
Eran Hammer <eran@hueniverse.com>
2012-11-24 00:00:10 -0800

    Payload tests and bug fix

--- test/unit/payload.js
+++ test/unit/payload.js
@@ -4,7 +4,6 @@ var expect = require('chai').expect;
 var Payload = process.env.TEST_COV ? require('../../lib-cov/payload') : require('../../lib/payload');
 var Route = process.env.TEST_COV ? require('../../lib-cov/route') : require('../../lib/route');
 var Server = process.env.TEST_COV ? require('../../lib-cov/server') : require('../../lib/server');
-var ServerMock = require('./mocks/server');
 var NodeUtil = require('util');
 var Events = require('events');
 var Shot = require('shot');
@@ -12,7 +11,7 @@ var Shot = require('shot');
 
 describe('Payload', function () {
 
-    var server = new Server();
+    var server = new Server({ payload: { maxBytes: 48 } });
 
     var shotRequest = function (method, path, headers, payload, callback) {
 
@@ -86,11 +85,11 @@ describe('Payload', function () {
 
                 var request = {
                     method: 'post',
-                    _route: new Route({ method: 'post', path: '/', handler: function () { } }, ServerMock),
+                    _route: new Route({ method: 'post', path: '/', handler: function () { } }, server),
                     raw: {
                         req: req
                     },
-                    server: ServerMock
+                    server: server
                 };
 
                 Payload.read(request, function (err) {
@@ -107,11 +106,11 @@ describe('Payload', function () {
 
                 var request = {
                     method: 'post',
-                    _route: new Route({ method: 'post', path: '/', handler: function () { } }, ServerMock),
+                    _route: new Route({ method: 'post', path: '/', handler: function () { } }, server),
                     raw: {
                         req: req
                     },
-                    server: ServerMock
+                    server: server
                 };
 
                 Payload.read(request, function (err) {
@@ -129,11 +128,11 @@ describe('Payload', function () {
 
                 var request = {
                     method: 'post',
-                    _route: new Route({ method: 'post', path: '/', handler: function () { } }, ServerMock),
+                    _route: new Route({ method: 'post', path: '/', handler: function () { } }, server),
                     raw: {
                         req: req
                     },
-                    server: ServerMock
+                    server: server
                 };
 
                 Payload.read(request, function (err) {
@@ -144,38 +143,48 @@ describe('Payload', function () {
             });
         });
 
-        /*    it('passes an Error to the callback whenever reading an invalid form request', function(done) {
-                function clientRequest() {
-                    Events.EventEmitter.call(this);
-                }
-    
-                NodeUtil.inherits(clientRequest, Events.EventEmitter);
-    
-                var req = new clientRequest();
-                req.headers = {
-                    'content-type': 'application/x-www-form-urlencoded'
-                };
-    
-                req.setEncoding = function() { };
-    
+        it('passes an Error to the callback whenever reading a payload too big (no header)', function (done) {
+
+            shotRequest('POST', '/', { 'content-type': 'application/json' }, '{ "key":"12345678901234567890123456789012345678901234567890" }', function (req, res) {
+
                 var request = {
                     method: 'post',
-                    _route: new Route({ method: 'post', path: '/', handler: function (){} }, {}),
+                    _route: new Route({ method: 'post', path: '/', handler: function () { } }, server),
                     raw: {
                         req: req
                     },
-                    server: ServerMock
+                    server: server
                 };
-    
-                Payload.read(request, function(err) {
+
+                Payload.read(request, function (err) {
                     expect(err).to.exist;
                     expect(request.payload).to.be.empty;
                     expect(err).to.be.an.instanceOf(Error);
                     done();
                 });
-    
-                req.emit('data', '\u777F');
-                req.emit('end');
-            });*/
+            });
+        });
+
+        it('passes an Error to the callback whenever reading a payload too big (header)', function (done) {
+
+            shotRequest('POST', '/', { 'content-type': 'application/json', 'content-length': 62 }, '{ "key":"12345678901234567890123456789012345678901234567890" }', function (req, res) {
+
+                var request = {
+                    method: 'post',
+                    _route: new Route({ method: 'post', path: '/', handler: function () { } }, server),
+                    raw: {
+                        req: req
+                    },
+                    server: server
+                };
+
+                Payload.read(request, function (err) {
+                    expect(err).to.exist;
+                    expect(request.payload).to.be.empty;
+                    expect(err).to.be.an.instanceOf(Error);
+                    done();
+                });
+            });
+        });
     });
 });
