27605159b3444e89a253c077e22c76b811f633b6
Eran Hammer <eran@hueniverse.com>
2013-01-08 08:34:06 -0800

    Views refactor

--- examples/views/basic.js
+++ examples/views/basic.js
@@ -2,16 +2,17 @@
 
 var Hapi = require('../../lib');
 
+
 // Declare internals
 
 var internals = {};
 
 
-var basicHandler = function (request) {
+var handler = function (request) {
 
     request.reply.view('basic/index', {
         title: 'examples/views/basic.js | Hapi ' + Hapi.utils.version(),
-        message: 'Hello World!\n'
+        message: 'Hello World!'
     }).send();
 };
 
@@ -20,7 +21,7 @@ internals.main = function () {
 
     var options = {
         views: {
-            path: __dirname + '/views',
+            path: __dirname + '/templates',
             engine: {
                 module: 'handlebars'
             }
@@ -28,7 +29,7 @@ internals.main = function () {
     };
 
     var server = new Hapi.Server(3000, options);
-    server.addRoute({ method: 'GET', path: '/', handler: basicHandler });
+    server.addRoute({ method: 'GET', path: '/', handler: handler });
     server.start();
 };
 
