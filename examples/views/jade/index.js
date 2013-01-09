26388c608634ffa7e1044c0dbf9ff57a94f41165
Van Nguyen <thegoleffect@gmail.com>
2013-01-09 11:05:44 -0800

    jade support and examplee

---
+++ examples/views/jade/index.js
@@ -0,0 +1,40 @@
+// Load modules
+
+var Hapi = require('../../../lib');
+
+
+// Declare internals
+
+var internals = {};
+
+
+var handler = function (request) {
+
+    request.reply.view('layout', {
+        title: 'examples/views/jade/index.js | Hapi ' + Hapi.utils.version(),
+        message: 'Hello World!'
+    }).send();
+};
+
+
+internals.main = function () {
+
+    var options = {
+        views: {
+            path: __dirname + '/templates',
+            engine: {
+                module: 'jade',
+                extension: 'jade'
+            },
+            filename: __dirname + '/templates/layout.jade',
+            pretty: true
+        }
+    };
+
+    var server = new Hapi.Server(3000, options);
+    server.addRoute({ method: 'GET', path: '/', handler: handler });
+    server.start();
+};
+
+
+internals.main();
