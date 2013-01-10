3af6c432102dbe88a9964a887e0e43b52aee973c
Eran Hammer <eran@hueniverse.com>
2013-01-09 16:19:20 -0800

    Fix proxy example back

--- examples/proxy.js
+++ examples/proxy.js
@@ -10,16 +10,11 @@ var internals = {};
 
 internals.main = function () {
 
-    var http = new Hapi.Server('0.0.0.0', 8080);
+    var http = new Hapi.Server(8080);
 
-    var map = function (request, callback) {
-
-        var url = 'http://api-groceries-qa2.asda.com/api/' + request.params.api + request.url.search;
-        console.log(url);
-        return callback(null, url);
-    };
-
-    http.addRoute({ method: 'GET', path: '/api/{api*}', handler: { proxy: { mapUri: map } } });
+    http.addRoute({ method: 'GET', path: '/', handler: { proxy: { host: 'google.com', port: 80 } } });
+    http.addRoute({ method: 'GET', path: '/images/srpr/logo3w.png', handler: { proxy: { host: 'google.com', port: 80 } } });
+    http.addRoute({ method: 'POST', path: '/', handler: { proxy: { host: 'google.com', port: 80 } } });
 
     http.start();
 };
