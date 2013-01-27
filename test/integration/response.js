d9b50c28b13ecec32fd09c4ef25147aa9eeba40e
Eran Hammer <eran@hueniverse.com>
2013-01-27 10:05:49 -0800

    Fix inconsistent raw api using send()

--- test/integration/response.js
+++ test/integration/response.js
@@ -136,7 +136,7 @@ describe('Response', function () {
 
     describe('Raw', function () {
 
-        it('returns a direct reply', function (done) {
+        it('returns a reply', function (done) {
 
             var handler = function (request) {
 
@@ -165,7 +165,35 @@ describe('Response', function () {
             });
         });
 
-        it('returns a direct reply with no payload', function (done) {
+        it('returns a reply using send()', function (done) {
+
+            var handler = function (request) {
+
+                var response = request.reply.raw();
+                response.type('text/plain')
+                        .bytes(13)
+                        .ttl(1000)
+                        .state('sid', 'abcdefg123456')
+                        .write('!hola ')
+                        .write('amigos!')
+                        .send();
+            };
+
+            var server = new Hapi.Server({ cors: { origin: ['test.example.com'] } });
+            server.addRoute({ method: 'GET', path: '/', config: { handler: handler, cache: { mode: 'client', expiresIn: 9999 } } });
+
+            server.inject({ method: 'GET', url: '/' }, function (res) {
+
+                expect(res.statusCode).to.equal(200);
+                expect(res.headers['set-cookie']).to.deep.equal(['sid=abcdefg123456']);
+                expect(res.readPayload()).to.equal('!hola amigos!');
+                expect(res.headers['cache-control']).to.equal('max-age=1, must-revalidate');
+                expect(res.headers['access-control-allow-origin']).to.equal('test.example.com');
+                done();
+            });
+        });
+
+        it('returns a reply with no payload', function (done) {
 
             var handler = function (request) {
 
@@ -191,7 +219,7 @@ describe('Response', function () {
             });
         });
 
-        it('returns a direct reply (created)', function (done) {
+        it('returns a reply (created)', function (done) {
 
             var handler = function (request) {
 
