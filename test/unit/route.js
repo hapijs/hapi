ded2c6d2b8928167336709a2486a0e2fb0852a3a
Eran Hammer <eran@hueniverse.com>
2013-02-10 10:54:27 -0800

    Revert "Merge pull request #474 from walmartlabs/develop"
    This reverts commit 0c91ecadb0e8be1c1467ce59cb44dea706736d3c, reversing
    changes made to 7e3a2a5fdf86b5883d26836cfc07d31e0c9dced5.

--- test/unit/route.js
+++ test/unit/route.js
@@ -232,11 +232,6 @@ describe('Route', function () {
                         p1: 'a',
                         p2: undefined
                     }
-                },
-                '/{p*}': {
-                    '/path/': {
-                        p: 'path/'
-                    }
                 }
             };
 
