9773e9c6c69c20cbd5f008c219ffba066357ad44
Eran Hammer <eran@hueniverse.com>
2013-03-12 17:42:46 -0700

    tests and error

--- test/unit/ext.js
+++ test/unit/ext.js
@@ -82,26 +82,56 @@ describe('Ext', function () {
             });
         });
 
-        it('sorts dependencies (2)', function (done) {
+        it('sorts dependencies (explicit)', function (done) {
 
-            var scenario = [
-                { id: '0', before: 'a' },
-                { id: '1', after: 'f', group: 'a' },
-                { id: '2', before: 'a' },
-                { id: '3', before: ['b', 'c'], group: 'a' },
-                { id: '4', after: 'c', group: 'b' },
-                { id: '5', group: 'c' },
-                { id: '6', group: 'd' },
-                { id: '7', group: 'e' },
-                { id: '8', before: 'd', after: 'e' },
-                { id: '9', after: 'c', group: 'a' }
-            ];
+            var set = '0123456789abcdefghijklmnopqrstuvwxyz';
+            var array = set.split('');
 
+            var scenario = [];
+            for (var i = 0, il = array.length; i < il; ++i) {
+                var item = {
+                    id: array[i],
+                    group: array[i],
+                    after: i ? array.slice(0, i) : [],
+                    before: array.slice(i + 1)
+                };
+                scenario.push(item);
+            }
+
+            var fisherYates = function (array) {
+
+                var i = array.length;
+                while (--i) {
+                    var j = Math.floor(Math.random() * (i + 1));
+                    var tempi = array[i];
+                    var tempj = array[j];
+                    array[i] = tempj;
+                    array[j] = tempi;
+                }
+            };
+
+            fisherYates(scenario);
             testDeps(scenario, function (result) {
 
-                expect(result).to.equal('0213547869');
+                expect(result).to.equal(set);
                 done();
             });
+        });
+
+        it('throws on circular dependency', function (done) {
+
+            var scenario = [
+                { id: '0', before: 'a', group: 'b'},
+                { id: '1', before: 'c', group: 'a' },
+                { id: '2', before: 'b', group: 'c' }
+            ];
+
+            expect(function () {
+
+                testDeps(scenario, function (result) { });
+            }).to.throw();
+
+            done();
         });
     });
 });
