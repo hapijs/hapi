fa9adb1b75c352ec3c4d6f32eeebb2016177774d
Eran Hammer <eran@hueniverse.com>
2013-07-03 08:49:56 -0700

    Fix #949, move to cryptiles

--- examples/stream.js
+++ examples/stream.js
@@ -1,6 +1,7 @@
 // Load modules
 
 var Hapi = require('../lib');
+var Request = require('request');
 
 
 // Declare internals
@@ -14,10 +15,18 @@ internals.echo = function () {
 };
 
 
+internals.request = function () {
+
+    var reqStream = Request('http://google.com');
+    reqStream.once('response', this.reply);
+};
+
+
 internals.main = function () {
 
     var server = new Hapi.Server(8000);
     server.route({ method: 'POST', path: '/', config: { handler: internals.echo, payload: 'stream' } });
+    server.route({ method: 'GET', path: '/request', handler: internals.request });
     server.start();
 };
 
