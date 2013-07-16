4b3fa05a7342997c6187e2fefb88f1692dfa666b
Wyatt Preul <wpreul@gmail.com>
2013-07-16 10:44:48 -0500

    Cleaining up dtrace to be more efficient

--- test/integration/dtrace.js
+++ test/integration/dtrace.js
@@ -21,9 +21,10 @@ var it = Lab.test;
 
 describe('DTrace', function () {
 
-    it('fires pre start and end probes when a request is made with prerequisites', function (done) {
+    it('doesn\'t fire probes when dtrace provider isn\'t installed', function (done) {
 
         var provider = DTrace.Provider;
+        var isInstalled = DTrace.isInstalled;
         DTrace.Provider = function () {
 
             return {
@@ -33,13 +34,15 @@ describe('DTrace', function () {
                     return {
                         fire: function (fn) {
 
-                            DTrace.Provider = provider;
-                            expect(fn()).to.contain('m1');
-                            done();
+                            expect(fn).to.not.exist;
                         }
                     };
                 }
             };
+        };
+        DTrace.isInstalled = function () {
+
+            return false;
         };
 
         var server = new Hapi.Server();
@@ -62,6 +65,55 @@ describe('DTrace', function () {
 
         server.inject({ url: '/' }, function () {
 
+            DTrace.Provider = provider;
+            DTrace.isInstalled = isInstalled;
+            done();
         });
+    });
+
+    it('fires correct probe on prerequisites when dtrace-provider is installed', function (done) {
+
+        var provider = DTrace.Provider;
+        var isInstalled = DTrace.isInstalled;
+        DTrace.Provider = function () {
+
+            return {
+                enable: function () {},
+                addProbe: function () {
+
+                    return {
+                        fire: function (fn) {
+
+                            expect(fn()).to.contain('m1');
+                            DTrace.Provider = provider;
+                            DTrace.isInstalled = isInstalled;
+                            done();
+                        }
+                    };
+                }
+            };
+        };
+        DTrace.isInstalled = function () {
+
+            return true;
+        };
+
+        var server = new Hapi.Server();
+        var pre1 = function (request, next) {
+
+            next('Hello');
+        };
+
+        server.route({ method: '*', path: '/', config: {
+            handler: function () {
+
+                this.reply('OK');
+            },
+            pre: [
+                { method: pre1, assign: 'm1' }
+            ]
+        }});
+
+        server.inject({ url: '/' }, function () {});
     });
 });
