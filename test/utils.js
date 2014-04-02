e525c79a93a9e0a08a32f2ea63d9b5240548f6c7
Andrew Burgess <andrew@deceptacle.com>
2014-04-01 18:29:48 -0700

    Added tests for checking isAbsolutePath

--- test/utils.js
+++ test/utils.js
@@ -29,4 +29,37 @@ describe('Utils', function () {
             done();
         });
     });
+
+    describe('#isAbsolutePath', function () {
+
+        it('returns true for absolute *nix path', function (done) {
+            var path = '/test/directory/file.js';
+            expect(Hapi.utils.isAbsolutePath(path)).to.equal(true);
+            done();
+        });
+
+        it('returns true for absolute Windows path', function (done) {
+            var path = 'C:\\Test\\Directory\\File.txt';
+            expect(Hapi.utils.isAbsolutePath(path)).to.equal(true);
+            done();
+        });
+
+        it('returns true for absolute azure path', function (done) {
+            var path = '\\\\server\\test\\directory\\file.js';
+            expect(Hapi.utils.isAbsolutePath(path)).to.equal(true);
+            done();
+        });
+
+        it('returns false for *nix relative path', function (done) {
+            var path = '../test/directory/test.js';
+            expect(Hapi.utils.isAbsolutePath(path)).to.equal(false);
+            done();
+        });
+
+        it('returns false for windows relative path', function (done) {
+            var path = '..\\test\\directory\\test.js';
+            expect(Hapi.utils.isAbsolutePath(path)).to.equal(false);
+            done();
+        })
+    });
 });
