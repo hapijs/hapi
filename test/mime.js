c66926e351d8583babc53b3a0a1391f03aeeb12a
Eran Hammer <eran@hueniverse.com>
2014-09-08 17:16:28 -0700

    Cleanup for #1905

--- test/mime.js
+++ test/mime.js
@@ -12,43 +12,32 @@ var internals = {};
 // Test shortcuts
 
 var lab = exports.lab = Lab.script();
-var before = lab.before;
-var after = lab.after;
 var describe = lab.describe;
 var it = lab.it;
 var expect = Lab.expect;
 
 describe('Mime', function () {
 
-    describe('fromPath', function () {
+    describe('#path', function () {
 
         it('returns the mime type from a file path', function (done) {
 
-            var result = Mime.fromPath('/static/javascript/app.js');
+            var result = Mime.path('/static/javascript/app.js');
 
             expect(result).deep.equal({
                 source: 'iana',
                 charset: 'UTF-8',
                 compressible: true,
                 extensions: [ 'js' ],
-                name: 'application/javascript'
+                type: 'application/javascript'
             });
             done();
         });
 
         it('returns false if a match can not be found', function (done) {
 
-            var result = Mime.fromPath('/static/javascript');
-
-            expect(result).to.equal(false);
-            done();
-        });
-
-        it ('returns false if path is not a string', function (done) {
-
-            var result = Mime.fromPath(10);
-
-            expect(result).to.equal(false);
+            var result = Mime.path('/static/javascript');
+            expect(result).to.deep.equal({});
             done();
         });
     });
