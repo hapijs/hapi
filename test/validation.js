edf18b2e1ae16d85fb62bd761baac59c8bf9202d
Nicolas Morel <nicolas@morel.io>
2017-09-21 12:04:25 +0200

    Support joi 11 paths arrays. Fixes #3581.

--- test/validation.js
+++ test/validation.js
@@ -614,6 +614,41 @@ describe('validation', () => {
         });
     });
 
+    it('fails on invalid input (with joi 11 error)', (done) => {
+
+        // Fake the joi 11 format
+        const joiFakeError = new Error();
+        joiFakeError.details = [{ path: ['foo', 'bar'] }];
+
+        const server = new Hapi.Server();
+        server.connection();
+        server.route({
+            method: 'GET',
+            path: '/',
+            handler: function (request, reply) {
+
+                return reply('ok');
+            },
+            config: {
+                validate: {
+                    query: {
+                        a: Joi.number().error(joiFakeError)
+                    }
+                }
+            }
+        });
+
+        server.inject('/?a=abc', (res) => {
+
+            expect(res.statusCode).to.equal(400);
+            expect(res.result.validation).to.equal({
+                source: 'query',
+                keys: ['foo.bar']
+            });
+            done();
+        });
+    });
+
     it('ignores invalid input', (done) => {
 
         const server = new Hapi.Server();
