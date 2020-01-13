d6960f221de3d6363b9d2a147b9c82cab67f8571
Eran Hammer <eran@hammer.io>
2020-01-13 02:31:45 +0000

    Retain  on 204 in inject. Closes #4021

--- test/response.js
+++ test/response.js
@@ -87,6 +87,14 @@ describe('Response', () => {
         expect(res.headers['content-type']).to.equal('text/plain; something=something; charset=utf-8');
     });
 
+    it('retains empty string', async () => {
+
+        const server = Hapi.server();
+        server.route({ method: 'GET', path: '/', handler: () => '' });
+        const res = await server.inject('/');
+        expect(res.result).to.equal('');
+    });
+
     describe('_setSource()', () => {
 
         it('returns an empty string response', async () => {
