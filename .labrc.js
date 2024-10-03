b720834ae8cb1d53f6317d0e9c0dde5dfab8b011
Gil Pedersen <git@gpost.dk>
2024-10-03 13:49:40 +0200

    Don't destroy incoming requests during the stopping phase

--- .labrc.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const Somever = require('@hapi/somever');
-
-module.exports = {
-    'coverage-predicates': {
-        allowsStoppedReq: Somever.match(process.version, '<18.19.0'),
-    }
-};
